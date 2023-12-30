f1100ecd8a63f633427a3f3598cfa2dea1ca009e
Shohel Rana <shohelrrrana@gmail.com>
2023-12-30 14:51:46 +0600

    implement 'position, width, wrapperClassName' props

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,61 +1,85 @@
-import { ReactNode } from "react"
-import { motion, AnimatePresence } from "framer-motion"
-import { RxCross2 as CancelIcon } from "react-icons/rx"
-import { createPortal } from "react-dom"
-import classes from "../styles/style.module.css"
+import { ReactNode } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+import { RxCross2 as CancelIcon } from 'react-icons/rx'
+import { createPortal } from 'react-dom'
+import classes from '../styles/style.module.css'
 
 export interface ModalProps {
-    visible: boolean,
+    visible: boolean
     toggle: () => void
     title?: string
     className?: string
+    wrapperClassName?: string
     hideIcon?: boolean
     children: ReactNode
+    position?: 'center' | 'top'
+    width?: string
 }
 
-export default function Modal( props: ModalProps ){
-    const { visible, toggle, title, children, hideIcon, className = '' } = props
+export default function Modal(props: ModalProps) {
+    const {
+        visible,
+        toggle,
+        title,
+        children,
+        hideIcon,
+        position,
+        width = '550px',
+        className = '',
+        wrapperClassName = '',
+    } = props
 
-    if( typeof window === "undefined" ) return null
+    if (typeof window === 'undefined') return null
 
-    const headerMarkup = ! hideIcon || title ? (
-        <header className={ classes.header }>
-            <h3 className={ classes.title }>{ title }</h3>
-            { ! hideIcon ? (
-                <button onClick={ toggle } className={ classes.icon }>
-                    <CancelIcon size="20"/>
-                </button>
-            ) : null }
-        </header>
-    ) : null
+    const headerMarkup =
+        !hideIcon || title ? (
+            <header className={classes.header}>
+                <h3 className={classes.title}>{title}</h3>
+                {!hideIcon ? (
+                    <button
+                        onClick={toggle}
+                        className={classes.icon}
+                    >
+                        <CancelIcon size='20' />
+                    </button>
+                ) : null}
+            </header>
+        ) : null
 
     const component = (
         <AnimatePresence>
-            { visible ? (
-                <div className={ classes.modal }>
+            {visible ? (
+                <div
+                    className={`${classes.modal} ${wrapperClassName}`}
+                    style={{
+                        justifyContent:
+                            position === 'top' ? 'flex-start' : 'center',
+                    }}
+                >
                     <motion.div
-                        className={ classes.darkOverlay }
-                        initial={ { opacity: 0 } }
-                        animate={ { opacity: 1 } }
-                        exit={ { opacity: 0 } }
-                        transition={ { duration: 0.2 } }
-                        onClick={ toggle }
+                        className={classes.darkOverlay}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.2 }}
+                        onClick={toggle}
                     />
                     <motion.div
-                        className={ classes.popup + ` ${ className }` }
-                        initial={ { opacity: 0, y: -10 } }
-                        animate={ { opacity: 1, y: 0 } }
-                        exit={ { opacity: 0, y: 10 } }
-                        transition={ { duration: 0.3 } }
+                        className={`${classes.popup} ${className}`}
+                        initial={{ opacity: 0, y: -10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: 10 }}
+                        transition={{ duration: 0.3 }}
+                        style={{ maxWidth: width }}
                     >
-                        { headerMarkup }
+                        {headerMarkup}
 
-                        { children }
+                        {children}
                     </motion.div>
                 </div>
-            ) : null }
+            ) : null}
         </AnimatePresence>
     )
 
-    return createPortal( component, document.body )
+    return createPortal(component, document.body)
 }
