634967efe0d569afe72c9c256e9fba875b54ff2c
Shohel Rana <shohelrrrana@gmail.com>
2024-02-11 19:48:58 +0600

    disable scrollbar when modal open

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -45,6 +45,18 @@ export default function Modal(props: ModalProps) {
    const [isClose, setIsClose] = useState(false)
 
    useEffect(() => {
+      if (open) {
+         document.body.style.overflow = 'hidden'
+      } else {
+         document.body.style.overflow = 'auto'
+      }
+
+      return () => {
+         document.body.style.overflow = 'auto'
+      }
+   }, [open])
+
+   useEffect(() => {
       if (open) return setIsClose(false)
 
       const timerId = setTimeout(() => {
